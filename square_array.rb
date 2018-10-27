475ee5a7324a7444a4641d8675cd06adc29fd3bf
kcluniesa <kcluniesa@yahoo.com>
2018-10-27 19:54:53 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,11 @@
+#def square_array(array)
+ # arr = []
+  #array.each { |i| arr << i ** 2 }
+  #return arr
+#end
+
 def square_array(array)
   arr = []
-  array.each { |i| arr << i ** 2 }
+  array.collect { |i| arr << i ** 2 }
   return arr
 end
-
